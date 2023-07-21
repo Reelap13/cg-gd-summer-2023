ce2b8e3ef0b66f694894331e74e3daa78af45f2f
Reelap13 <116920397+Reelap13@users.noreply.github.com>
2023-07-21 20:19:52 +0300

    Bug fixing commit

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -158,6 +158,38 @@ void cg::renderer::dx12_renderer::create_render_target_views()
 
 void cg::renderer::dx12_renderer::create_depth_buffer()
 {
+	CD3DX12_RESOURCE_DESC depth_buffer_desc(
+			D3D12_RESOURCE_DIMENSION_TEXTURE2D,
+			0,
+			settings->width,
+			settings->height,
+			1, 1,
+			DXGI_FORMAT_D32_FLOAT,
+			1, 0,
+			D3D12_TEXTURE_LAYOUT_UNKNOWN,
+			D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL |
+					D3D12_RESOURCE_FLAG_DENY_SHADER_RESOURCE);
+
+	D3D12_CLEAR_VALUE depth_clear_value{};
+	depth_clear_value.Format = DXGI_FORMAT_D32_FLOAT;
+	depth_clear_value.DepthStencil.Depth = 1.f;
+	depth_clear_value.DepthStencil.Stencil = 0;
+
+	THROW_IF_FAILED(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+			D3D12_HEAP_FLAG_NONE,
+			&depth_buffer_desc,
+			D3D12_RESOURCE_STATE_DEPTH_WRITE,
+			&depth_clear_value,
+			IID_PPV_ARGS(&depth_buffer)));
+	depth_buffer->SetName(L"Depth buffer");
+
+	dsv_heap.create_heap(device, D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+
+	device->CreateDepthStencilView(
+			depth_buffer.Get(),
+			nullptr,
+			dsv_heap.get_cpu_descriptor_handle());
 }
 
 void cg::renderer::dx12_renderer::create_command_allocators()
@@ -189,18 +221,34 @@ void cg::renderer::dx12_renderer::load_pipeline()
 	create_direct_command_queue();
 	create_swap_chain(dxgi_factory);
 	create_render_target_views();
+	create_depth_buffer();
+
 }
 
 D3D12_STATIC_SAMPLER_DESC cg::renderer::dx12_renderer::get_sampler_descriptor()
 {
 	D3D12_STATIC_SAMPLER_DESC sampler_desc{};
+	sampler_desc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	sampler_desc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	sampler_desc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	sampler_desc.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
+	sampler_desc.MaxLOD = D3D12_FLOAT32_MAX;
+	sampler_desc.MinLOD = 0;
+	sampler_desc.MipLODBias = 0;
+	sampler_desc.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+	sampler_desc.Filter = D3D12_FILTER_ANISOTROPIC;
+	sampler_desc.MaxAnisotropy = 16;
+	sampler_desc.ShaderRegister = 0;
+	sampler_desc.RegisterSpace = 0;
+	sampler_desc.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+
 	return sampler_desc;
 }
 
 void cg::renderer::dx12_renderer::create_root_signature(const D3D12_STATIC_SAMPLER_DESC* sampler_descriptors, UINT num_sampler_descriptors)
 {
-	CD3DX12_ROOT_PARAMETER1 root_parameters[1];
-	CD3DX12_DESCRIPTOR_RANGE1 ranges[1];
+	CD3DX12_ROOT_PARAMETER1 root_parameters[2];
+	CD3DX12_DESCRIPTOR_RANGE1 ranges[2];
 	
 	ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 0, 0,
 				   D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
@@ -208,6 +256,14 @@ void cg::renderer::dx12_renderer::create_root_signature(const D3D12_STATIC_SAMPL
 			1,
 			&ranges[0],
 			D3D12_SHADER_VISIBILITY_ALL);
+
+	ranges[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0,
+				   D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
+	root_parameters[1].InitAsDescriptorTable(
+			1,
+			&ranges[1],
+			D3D12_SHADER_VISIBILITY_PIXEL);
+
 
 	D3D12_FEATURE_DATA_ROOT_SIGNATURE rs_feature_data{};
 	rs_feature_data.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
@@ -299,6 +355,10 @@ void cg::renderer::dx12_renderer::create_pso(const std::string& shader_name)
 			get_shader_path(shader_name),
 			"PSMain",
 			"ps_5_0");
+	ComPtr<ID3DBlob> pixel_shader_texture = compile_shader(
+			get_shader_path(shader_name),
+			"PSMain_texture",
+			"ps_5_0");
 
 	D3D12_INPUT_ELEMENT_DESC input_descs[] = {
 			{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,
@@ -325,17 +385,25 @@ void cg::renderer::dx12_renderer::create_pso(const std::string& shader_name)
 	pso_desc.RasterizerState.FrontCounterClockwise = TRUE;
 	pso_desc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID; // can use Warframe for not full filling
 	pso_desc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
-	pso_desc.DepthStencilState.DepthEnable = FALSE;
+	pso_desc.DepthStencilState.DepthEnable = TRUE;
 	pso_desc.DepthStencilState.StencilEnable = FALSE;
+	pso_desc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+	pso_desc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
 	pso_desc.SampleMask = UINT_MAX;
 	pso_desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 	pso_desc.NumRenderTargets = 1;
 	pso_desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+	pso_desc.DSVFormat = DXGI_FORMAT_D32_FLOAT;
 	pso_desc.SampleDesc.Count = 1;
 
 	THROW_IF_FAILED(device->CreateGraphicsPipelineState(
 			&pso_desc,
 			IID_PPV_ARGS(&pipeline_state)));
+
+	pso_desc.PS = CD3DX12_SHADER_BYTECODE(pixel_shader_texture.Get());
+	THROW_IF_FAILED(device->CreateGraphicsPipelineState(
+			&pso_desc,
+			IID_PPV_ARGS(&pipeline_state_texture)));
 }
 
 void cg::renderer::dx12_renderer::create_resource_on_upload_heap(
@@ -356,11 +424,32 @@ void cg::renderer::dx12_renderer::create_resource_on_upload_heap(
 	}
 }
 
-void cg::renderer::dx12_renderer::create_resource_on_default_heap(ComPtr<ID3D12Resource>& resource, UINT size, const std::wstring& name, D3D12_RESOURCE_DESC* resource_descriptor)
+void cg::renderer::dx12_renderer::create_resource_on_default_heap(
+	ComPtr<ID3D12Resource>& resource, 
+	UINT size, const std::wstring& name, 
+	D3D12_RESOURCE_DESC* resource_descriptor)
 {
+	if (resource_descriptor == nullptr)
+	{
+		resource_descriptor = &CD3DX12_RESOURCE_DESC::Buffer(size);
+	}
+	THROW_IF_FAILED(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+			D3D12_HEAP_FLAG_NONE,
+			resource_descriptor,
+			D3D12_RESOURCE_STATE_COPY_DEST,
+			nullptr,
+			IID_PPV_ARGS(&resource)));
+	if (!name.empty())
+	{
+		resource->SetName(name.c_str());
+	}
 }
 
-void cg::renderer::dx12_renderer::copy_data(const void* buffer_data, UINT buffer_size, ComPtr<ID3D12Resource>& destination_resource)
+void cg::renderer::dx12_renderer::copy_data(
+	const void* buffer_data, 
+	UINT buffer_size, 
+	ComPtr<ID3D12Resource>& destination_resource)
 {
 	UINT8* buffer_data_begin;
 	CD3DX12_RANGE read_range(0, 0);
@@ -370,8 +459,28 @@ void cg::renderer::dx12_renderer::copy_data(const void* buffer_data, UINT buffer
 	destination_resource->Unmap(0, 0);
 }
 
-void cg::renderer::dx12_renderer::copy_data(const void* buffer_data, const UINT buffer_size, ComPtr<ID3D12Resource>& destination_resource, ComPtr<ID3D12Resource>& intermediate_resource, D3D12_RESOURCE_STATES state_after, int row_pitch, int slice_pitch)
+void cg::renderer::dx12_renderer::copy_data(
+	const void* buffer_data, 
+	const UINT buffer_size, 
+	ComPtr<ID3D12Resource>& destination_resource, 
+	ComPtr<ID3D12Resource>& intermediate_resource,
+	D3D12_RESOURCE_STATES state_after, 
+	int row_pitch, int slice_pitch)
 {
+	D3D12_SUBRESOURCE_DATA data{};
+	data.pData = buffer_data;
+	data.RowPitch = row_pitch != 0 ? row_pitch : buffer_size;
+	data.SlicePitch = slice_pitch != 0 ? slice_pitch : buffer_size;
+
+	UpdateSubresources(command_list.Get(), destination_resource.Get(),
+					   intermediate_resource.Get(), 0, 0, 1, &data);
+
+	command_list->ResourceBarrier(
+			1,
+			&CD3DX12_RESOURCE_BARRIER::Transition(
+					destination_resource.Get(),
+					D3D12_RESOURCE_STATE_COPY_DEST,
+					state_after));
 }
 
 D3D12_VERTEX_BUFFER_VIEW cg::renderer::dx12_renderer::create_vertex_buffer_view(const ComPtr<ID3D12Resource>& vertex_buffer, const UINT vertex_buffer_size)
@@ -394,6 +503,13 @@ D3D12_INDEX_BUFFER_VIEW cg::renderer::dx12_renderer::create_index_buffer_view(co
 
 void cg::renderer::dx12_renderer::create_shader_resource_view(const ComPtr<ID3D12Resource>& texture, D3D12_CPU_DESCRIPTOR_HANDLE cpu_handler)
 {
+	D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc{};
+	srv_desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srv_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	srv_desc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srv_desc.Texture2D.MipLevels = 1;
+	
+	device->CreateShaderResourceView(texture.Get(), &srv_desc, cpu_handler);
 }
 
 void cg::renderer::dx12_renderer::create_constant_buffer_view(const ComPtr<ID3D12Resource>& buffer, D3D12_CPU_DESCRIPTOR_HANDLE cpu_handler)
@@ -407,15 +523,18 @@ void cg::renderer::dx12_renderer::create_constant_buffer_view(const ComPtr<ID3D1
 
 void cg::renderer::dx12_renderer::load_assets()
 {
-	create_root_signature(nullptr, 0);
+	auto sampler_desc = get_sampler_descriptor();
+	create_root_signature(&sampler_desc, 1);
 	create_pso("shaders.hlsl");
 	create_command_allocators();
 	create_command_list();
 
 	vertex_buffers.resize(model->get_vertex_buffers().size());
+    upload_vertex_buffers.resize(model->get_vertex_buffers().size());
 	vertex_buffer_views.resize(model->get_vertex_buffers().size());
 
 	index_buffers.resize(model->get_index_buffers().size());
+	upload_index_buffers.resize(model->get_index_buffers().size());
 	index_buffer_views.resize(model->get_index_buffers().size());
 
 	for (size_t i = 0; i < model->get_index_buffers().size(); ++i)
@@ -426,13 +545,18 @@ void cg::renderer::dx12_renderer::load_assets()
 				vertex_buffer_data->get_size_in_bytes());
 		std::wstring vertex_buffer_name(L"Vertex buffer ");
 		vertex_buffer_name += std::to_wstring(i);
-		create_resource_on_upload_heap(
+		create_resource_on_default_heap(
 				vertex_buffers[i],
 				vertex_buffer_size,
 				vertex_buffer_name);
+		create_resource_on_upload_heap(
+				upload_vertex_buffers[i],
+				vertex_buffer_size);
 		copy_data(vertex_buffer_data->get_data(),
 				  vertex_buffer_size,
-				  vertex_buffers[i]);
+				  vertex_buffers[i],
+				  upload_vertex_buffers[i],
+				  D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
 		vertex_buffer_views[i] = create_vertex_buffer_view(
 				vertex_buffers[i],
 				vertex_buffer_size);
@@ -443,13 +567,18 @@ void cg::renderer::dx12_renderer::load_assets()
 				index_buffer_data->get_size_in_bytes());
 		std::wstring index_buffer_name(L"Index buffer ");
 		index_buffer_name += std::to_wstring(i);
-		create_resource_on_upload_heap(
+		create_resource_on_default_heap(
 				index_buffers[i],
 				index_buffer_size,
 				index_buffer_name);
+		create_resource_on_upload_heap(
+				upload_index_buffers[i],
+				index_buffer_size);
 		copy_data(index_buffer_data->get_data(),
 				  index_buffer_size,
-				  index_buffers[i]);
+				  index_buffers[i],
+				  upload_index_buffers[i],
+				  D3D12_RESOURCE_STATE_INDEX_BUFFER);
 		index_buffer_views[i] = create_index_buffer_view(
 				index_buffers[i],
 				index_buffer_size);
@@ -474,14 +603,66 @@ void cg::renderer::dx12_renderer::load_assets()
 	cbv_srv_heap.create_heap(
 			device,
 			D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-			1,
+			1 + static_cast<UINT>(model->get_per_shape_texture_files().size()),
 			D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE);
 
 	create_constant_buffer_view(
 			constant_buffer,
 			cbv_srv_heap.get_cpu_descriptor_handle(0));
 
+
+	textures.resize(model->get_per_shape_texture_files().size());
+	upload_textures.resize(model->get_per_shape_texture_files().size());
+
+	for (size_t i = 0; i < model->get_per_shape_texture_files().size(); i++) {
+		if (model->get_per_shape_texture_files()[i].empty()) {
+			continue;
+		}
+		std::string full_name = std::filesystem::absolute(
+										model->get_per_shape_texture_files()[i])
+										.string();
+		int tex_width, tex_height, tex_channels;
+		unsigned char* image = stbi_load(
+				full_name.c_str(),
+				&tex_width,
+				&tex_height,
+				&tex_channels,
+				STBI_rgb_alpha);
+		if (image == nullptr) {
+			throw std::runtime_error("Can't load a texture");
+		}
+		D3D12_RESOURCE_DESC texture_desc{};
+		texture_desc.MipLevels = 1;
+		texture_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		texture_desc.Width = tex_width;
+		texture_desc.Height = tex_height;
+		texture_desc.DepthOrArraySize = 1;
+		texture_desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+		texture_desc.Flags = D3D12_RESOURCE_FLAG_NONE;
+		texture_desc.SampleDesc.Count = 1;
+		texture_desc.SampleDesc.Quality = 0;
+
+		create_resource_on_default_heap(
+				textures[i], 0,
+				model->get_per_shape_texture_files()[i],
+				&texture_desc);
+		const UINT upload_buffer_size = static_cast<UINT>(GetRequiredIntermediateSize(textures[i].Get(),
+																					  0, 1));
+		create_resource_on_upload_heap(upload_textures[i],
+									   upload_buffer_size);
+		copy_data(image, upload_buffer_size, textures[i],
+				  upload_textures[i], D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+				  tex_width * STBI_rgb_alpha,
+				  tex_height * tex_width * STBI_rgb_alpha);
+		create_shader_resource_view(
+				textures[i],
+				cbv_srv_heap.get_cpu_descriptor_handle(static_cast<UINT>(i + 1)));
+	}
+
 	THROW_IF_FAILED(command_list->Close());
+	ID3D12CommandList* command_lists[] = {command_list.Get()};
+	command_queue->ExecuteCommandLists(_countof(command_lists),
+									   command_lists);
 
 	//Create a fenct
 	THROW_IF_FAILED(device->CreateFence(
@@ -527,11 +708,15 @@ void cg::renderer::dx12_renderer::populate_command_list()
 			1,
 			&rtv_heap.get_cpu_descriptor_handle(frame_index),
 			FALSE,
-			nullptr);
+			&dsv_heap.get_cpu_descriptor_handle());
 	const float clear_color[] = {0.f, 0.f, 0.f, 1.f};
 	command_list->ClearRenderTargetView(
 			rtv_heap.get_cpu_descriptor_handle(frame_index),
 			clear_color, 0, nullptr);
+	command_list->ClearDepthStencilView(
+			dsv_heap.get_cpu_descriptor_handle(),
+			D3D12_CLEAR_FLAG_DEPTH,
+			1.f, 0, 0, nullptr);
 
 	for (size_t s = 0; s < model->get_index_buffers().size(); ++s)
 	{
